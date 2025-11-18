9ca20e2f105926f11f49bbb2b29c44d08ae7a803
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-11-18 13:25:58 +0530

    Create Reverse-String.py

---
+++ PYTHON/Basic-Codes/Reverse-String.py
@@ -0,0 +1,21 @@
+
+st = input('enter a string : ')
+print(st[::-1])
+
+Output:
+Hello
+olleH
+
+-----------------------
+
+
+st = input('enter a string : ')
+s = st.split(' ')
+
+print(s[::-1])
+
+
+
+Output :
+enter a string : Hello World
+['World', 'Hello']
