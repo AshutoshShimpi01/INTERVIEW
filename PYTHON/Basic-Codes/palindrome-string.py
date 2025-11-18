8842216e967af02b484b59745f572f36984a21d8
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-11-18 13:33:19 +0530

    Create palindrome-string.py

---
+++ PYTHON/Basic-Codes/palindrome-string.py
@@ -0,0 +1,17 @@
+
+
+
+st = input('enter a string : ')
+
+if st.lower() == st[::-1].lower():
+    print('palindrome')
+    
+else:
+    print('not palindrome')
+
+
+
+out :
+
+enter a string : nayan
+palindrome
