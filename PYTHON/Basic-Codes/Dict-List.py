61e73a380c470c0eef55694db35439554de9896d
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-11-18 14:47:29 +0530

    Create Dict-List.py

---
+++ PYTHON/Basic-Codes/Dict-List.py
@@ -0,0 +1,21 @@
+
+
+
+
+ls = {
+    'name' : ['ashu','keshu','shri'],
+    'age' : [20,22,27],
+    'gender' : ['M','M','M']
+}
+
+print(ls['name'])
+print(ls['age'])
+print(ls['gender'])
+
+
+
+output :
+
+['ashu', 'keshu', 'shri']
+[20, 22, 27]
+['M', 'M', 'M']
