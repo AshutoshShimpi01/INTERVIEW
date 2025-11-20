ae43561f090fcbb47d674a1d5a08a41a40d54d8d
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-11-20 15:56:04 +0530

    Update String-Count.py

--- PYTHON/Basic-Codes/String-Count.py
+++ PYTHON/Basic-Codes/String-Count.py
@@ -10,6 +10,11 @@ result = ' '.join([
 print(st)
 print(result)
 
+o/t :
+Enter a string : i am ashutosh
+I Am Ashutosh
+
+-------------------------
 
 st = input("Enter a string: ")
 
@@ -19,12 +24,29 @@ result = st.capitalize()
 print(st)
 print(result)
 
-
+o/t:
+Enter a string: i am Ashutosh
+I am ashutosh
+----------------------------
 
 st = input("Enter a string: ")
 
 result = st[0].upper() + st[1:]
 print(result)
 
+o/t:
+Enter a string: i am ashu
+I am ashu
+
+-----------------------------
 
 
+from collections import *
+st = input('Enter string : ')
+ch = st.split()
+newString = ch[0].upper() + ' '+ ' '.join(ch[1::]).lower()
+print(newString)
+
+output:
+Enter string : i am ashu
+I am ashu
