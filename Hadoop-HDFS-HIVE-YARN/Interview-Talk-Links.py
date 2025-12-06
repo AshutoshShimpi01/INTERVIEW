342160f98700332145299658cc4fe4794c08ed09
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-12-06 14:52:09 +0530

    Create Interview-Talk-Links.py

---
+++ Hadoop-HDFS-HIVE-YARN/Interview-Talk-Links.py
@@ -0,0 +1,16 @@
+
+ALL :
+
+https://docs.google.com/spreadsheets/d/1FsgXZ2yeQHCZpAAAjErX74osFIskdyuqIN0EXHD40Bk/edit?gid=974218214#gid=974218214
+
+
+GCP :
+
+
+https://docs.google.com/spreadsheets/d/1GI0c1QlxR9cGKcFg2LoA_XnhRtt8ZbJubOHYkEvlAXA/edit?gid=1036697775#gid=1036697775
+
+
+PowerBi :
+
+
+https://docs.google.com/spreadsheets/d/1UD3u2YJ4H3XdAWU_KzUTVV4AMpfr2PIYWB5aR86WmAE/edit?gid=1000672373#gid=1000672373
