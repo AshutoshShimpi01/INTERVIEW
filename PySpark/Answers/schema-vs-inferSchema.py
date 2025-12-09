4f9f00def5fcc9a1685416e78d24860451fbf448
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-12-09 16:49:16 +0530

    Update schema-vs-inferSchema.py

--- PySpark/Answers/schema-vs-inferSchema.py
+++ PySpark/Answers/schema-vs-inferSchema.py
@@ -8,3 +8,25 @@
 | Reliability        | ❌ Can guess wrong       | ✅ Always correct        |
 | Production use     | ❌ Never                 | ✅ Always                |
 | Code               | Simple                  | Needs schema definition |
+
+
+
+
+
+
+# IMPLICIT
+
+data = [('ashu',25),('keshu',20),('shru',22),('yogu',29)]
+
+# NO column names provided = IMPLICIT
+df = spark.createDataFrame(data)  # Spark guesses: _1, _2
+df.show()
+
+
+
+# EXPLICIT
+
+data = [('ashu',25),('keshu',20),('shru',22),('yogu',29)]
+column = ['Name','age']  # ← YOU define column names & types
+
+df = spark.createDataFrame(data, column)  # ← Explicit schema
