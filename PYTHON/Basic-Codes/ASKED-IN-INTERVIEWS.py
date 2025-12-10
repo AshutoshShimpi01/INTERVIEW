5993f9a7da1bcd3c3fa20929b0104f5bfec86cf1
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-12-10 22:46:27 +0530

    Create ASKED-IN-INTERVIEWS.py

---
+++ PYTHON/Basic-Codes/ASKED-IN-INTERVIEWS.py
@@ -0,0 +1,64 @@
+
+-- STRING Problem Infos     ( aaabbc ---> a3b2c1 )
+
+from collections import *
+
+st = input('Enter a String : ')
+output = ''
+
+counts = Counter(st)
+
+for char, count in counts.items():
+    output += char + str(count)
+    
+print(output)
+
+Enter a String : aaabbc
+a3b2c1
+
+
+
+-----------------------------------------------
+
+
+-- STRING Problem Impets     ( apple is red ---> Apple Is Red )
+
+st = input('Enter a String : ')
+
+result = ' '.join([
+    
+    word[0].upper() + word[1::].lower()
+    for word in st.split() if word
+    
+    ])
+    
+print(result)
+
+Enter a String : apple is red
+Apple Is Red
+
+
+-----------------------------------------------
+
+
+-- REMOVE DUPLICATES inside List
+
+from collections import *
+
+
+A = [1,3,2,4,1,5,3,2,5]
+
+res = []
+temp = set()
+
+for i in A:
+    if i not in res:
+        res.append(i)
+        temp.add(i)
+        
+print(res)
+
+
+
+-----------------------------------------------
+
