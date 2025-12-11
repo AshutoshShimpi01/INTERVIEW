8d3edf9e12c49ab9e1584ccef15ec33c1f2679d0
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-12-11 21:03:22 +0530

    Update My-interviews-asked-Qs.py

--- SQL/My-interviews-asked-Qs.py
+++ SQL/My-interviews-asked-Qs.py
@@ -7,8 +7,9 @@ using EXISTS : -
 select name from employees e where  EXISTS      -- correlated subquery
 (select 1 from departments d where e.dept_id = d.dept_id and location = 'New York');
 
+------------------------------------
 
-
+INFOY
 
 -- second highest salary from emp table
 
@@ -23,8 +24,9 @@ from ch
 where rk = 2;     -- here 3rd, 4th, 5th any number of highest 
 
 
+------------------------------------
 
-
+ONIX
 
 
 -- find all employees whose salary is greater than the average salary of all employees in the table
@@ -33,8 +35,9 @@ select * from employees where salary > (select avg(salary) as av from employees)
 
 
 
+------------------------------------
 
-
+ZS
 
 
 -- Top 3 customers by total spending in year 2024 for each region.
@@ -57,3 +60,53 @@ from total
 select *
 from rr
 where rk< 4;
+
+
+------------------------------------
+DROP DUPLICATES
+
+
+WITH DuplicateCTE AS 
+(
+    SELECT ID, 
+      ROW_NUMBER() OVER(PARTITION BY Name ORDER BY ID ASC) as rn
+    FROM YourTable
+)
+DELETE FROM
+    YourTable
+WHERE ID IN (SELECT ID FROM DuplicateCTE WHERE rn > 1);
+
+
+------------------------------------
+UPDATE COLUMN
+
+update Orders
+set customer_id = 
+CASE
+WHEN customer_id = 4 THEN 3
+WHEN customer_id = 3 THEN 4
+ELSE customer_id END;
+
+
+------------------------------------
+
+IMPT
+
+SELECT
+    SUM(CASE WHEN Value > 0 THEN Value ELSE 0 END) AS Pos_Sum,
+    SUM(CASE WHEN Value < 0 THEN Value ELSE 0 END) AS Neg_Sum
+FROM
+    FinancialTransactions;
+
+
+------------------------------------
+
+
+
+
+
+
+
+------------------------------------
+
+
