31500bf9fb556dc4dccc1c2497eea56ae17a6dc9
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-12-11 16:57:56 +0530

    Update Fatak-Pay-List-Duplicate.py

--- PYTHON/Basic-Codes/Fatak-Pay-List-Duplicate.py
+++ PYTHON/Basic-Codes/Fatak-Pay-List-Duplicate.py
@@ -4,12 +4,26 @@ A = [1,4,2,4,7,3,8,9,7,5]
 
 check = []
 
-item = set()
-
 for i in A:
     if i not in check:
         check.append(i)
-        item.add(i)
         
-print(check)    #  [1, 4, 2, 7, 3, 8, 9, 5]
+print(check)    #  [1, 4, 2, 7, 3, 8, 9, 5]      
 
+
+
+
+
+BOTH WORKING SAME
+
+
+
+A = [1,2,3,2,3,2,4,6,4,7,1]
+B = []
+
+for i in A:
+    if i not in B:
+        B.append(i)
+print(B)
+
+#  [1, 2, 3, 4, 6, 7]
